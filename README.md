b7e3890c9342e9e62584edf5f92e6b12a50e4e78
Kenny <kenny02238@yahoo.com.tw>
2022-06-29 09:06:57 +0800

    READMEupdate

---
+++ README.md
@@ -0,0 +1,3 @@
+# 純 CSS.HTML 測試作品
+
+[觀看網站](https://kenny02238.github.io/Profile/)
---
+++ index.js
@@ -0,0 +1,10 @@
+// let title = document.getElementsByClassName('flex-title');
+// let content = document.getElementsByClassName('flex-content');
+// let flexWrapper = document.getElementsByClassName('flex-wrapper');
+
+// let change = function(e){
+//     content[e].style.opcitity='1';
+//     title[e].style.transform = 'rotate(0deg)';
+//     flexWrapper[e].style.flexgrow = '3';
+//     console.log(e);
+// };
